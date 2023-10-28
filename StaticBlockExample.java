7fc523937be7ac0b1afb18e0bca18a17abdd48d5
DTG-J <grozdanovdt@abv.bg>
2023-10-28 18:44:07 +0300

    Create StaticBlockExample.java

---
+++ StaticBlockExample.java
@@ -0,0 +1,20 @@
+package OOPJavaAdvanced;
+
+public class StaticBlockExample {
+    public static void main(String[] args) {
+
+        System.out.println("I am main " + System.getProperties().getProperty("java.runtime.version"));
+    }
+
+
+    static {
+        System.out.println("I am static block 1");
+    }
+
+    static {
+        System.out.println("I am static block 2");
+    }
+}
+
+
+
