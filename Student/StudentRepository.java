b9836f5c9df42120cdc06fab50f456b347e686a6
DTG-J <grozdanovdt@abv.bg>
2023-10-29 13:01:46 +0200

    Requires several classes and methods

---
+++ Student/StudentRepository.java
@@ -0,0 +1,18 @@
+package OOPJavaAdvanced.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentRepository {
+    private final Map <String, Student> studentByName;
+
+    public StudentRepository (){
+        this.studentByName = new HashMap<>();
+    }
+    public void create (Student student){
+        studentByName.putIfAbsent(student.getName(),student);
+    }
+    public Student get (String name){
+        return studentByName.get(name);
+    }
+}
