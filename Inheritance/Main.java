e12a0c72d6887f322e8fd221cdddccf4d25635c4
DTG-J <grozdanovdt@abv.bg>
2023-11-04 13:29:37 +0200

    Create Main.java

---
+++ Inheritance/Main.java
@@ -0,0 +1,15 @@
+package OOPJavaAdvanced.Java_OOP_Inheritance_Lab;
+
+import OOPJavaAdvanced.CardSuits.CardSuits;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat ();
+        dog.bark();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+    }
+}
