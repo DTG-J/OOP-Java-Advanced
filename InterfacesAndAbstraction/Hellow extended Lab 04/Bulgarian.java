63edd8187f576bfe600755aee2a7bdba0cca3bf6
DTG-J <grozdanovdt@abv.bg>
2023-11-05 20:24:57 +0200

    Create Bulgarian.java

---
+++ InterfacesAndAbstraction/Hellow extended Lab 04/Bulgarian.java
@@ -0,0 +1,11 @@
+public class Bulgarian extends BasePerson{
+    public Bulgarian(String name) {
+        super(name);
+
+    }
+
+    @Override
+    public String sayHellow() {
+        return "Здравей";
+    }
+}
