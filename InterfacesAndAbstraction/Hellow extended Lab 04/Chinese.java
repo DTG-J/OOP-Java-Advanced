028491b484c689c408f0147025edd80ac45845d6
DTG-J <grozdanovdt@abv.bg>
2023-11-05 20:24:41 +0200

    Create Chinese.java

---
+++ InterfacesAndAbstraction/Hellow extended Lab 04/Chinese.java
@@ -0,0 +1,11 @@
+public class Chinese extends BasePerson{
+     public Chinese(String name) {
+       super(name);
+
+    }
+
+    @Override
+    public String sayHellow() {
+        return "Djydjybydjy";
+    }
+}
